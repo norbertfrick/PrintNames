86311c3b52ccae05051d2a62f60c557bf84e02e2
Norbert Frick <norbert.frick.dev@itvkurze.sk>
2021-12-12 11:38:57 +0100

    first commit

---
+++ PrintNames/PrintNames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfNames = new List<string>()
+            {
+                "Norbert Frick"
+            };
+
+            foreach (var name in listOfNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
