d8e701de33aa2147e4f84698044666e6f2aa35b4
MiaFiolekova <Mia.fiolekova@centrum.sk>
2022-06-17 15:12:14 +0200

    another name

---
+++ PrintNames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfNames = new List<string>()
+            {
+                "Norbert Frick",
+                "Matus Benus",
+                "Stavri Benedikty",
+                "Peter Truchly",
+                "Mia Fiolekova"
+            };
+
+            foreach (var name in listOfNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
